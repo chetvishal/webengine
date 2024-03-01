419f4489de3544bbcb9b4d9133361f53d0899218
chetvishal <vishal1982c@gmail.com>
2024-03-01 11:41:58 +0530

    change in rule

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
       'Cross-Origin-Embedder-Policy': 'require-corp',
-      'Cross-Origin-Resource-Policy': 'same-site',
+      'Cross-Origin-Resource-Policy': 'crossorigin',
     },
   },
 })
